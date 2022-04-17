da782d7a5951143296eb26f7ed6cdf0e82751bb3
Adolfo Ramos <ingadolfo@icloud.com>
2022-04-16 19:57:54 -0500

    Add project files.

---
+++ DeliveryProducts/DeliveryProducts/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace DeliveryProducts.ViewModels
+{
+    public class HomePageViewModel : BindableObject
+    {
+        public HomePageViewModel()
+        {
+            BindingContext = this;
+
+            ShowBussines = new Command(OnShowBussines);
+        }
+
+        void OnShowBussines()
+        {
+            Bussines = "Mostrando negocios......";
+        }
+
+        public ICommand ShowBussines { get; }
+
+        private string bussines = "Todos los negocios";
+
+        public string Bussines
+        {
+            get => bussines;
+
+            set 
+            {
+                if (value == bussines)
+                    return;
+
+                bussines = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
